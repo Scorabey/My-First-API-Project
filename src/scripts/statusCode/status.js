ef312f5760d2c5cd8a6248b93389017f5514be99
Scorabey <satlovdanijl@gmail.com>
2025-12-17 10:20:56 +0200

    Delete status code

--- src/scripts/statusCode/status.js
+++
@@ -1,7 +0,0 @@
-export default function chakingStatus(error) {
-    if(error.status !== 429) {
-        console.log('API Working!')
-    } else {
-        throw new Error('API Status: 429, too many request!')
-    }
-}
